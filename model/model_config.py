33cfc2812f10bce21cb4f478c27066abe2d91b0d
Ivan Chukhran <ivan.chuhran@gmail.com>
2025-04-04 18:43:40 +0300

    add initial transformer implementation

--- model/model_config.py
+++ model/model_config.py
@@ -7,9 +7,11 @@ from torch.nn import functional as F
 
 class ModelConfig(BaseModel):
     model_config = ConfigDict(extra="allow")
+    num_layers: int = 3
     num_heads: int = 6
     num_embeddings: int = 512
-    block_size: int = 10
+    block_size: int = 128
+    vocab_size: int = 2048
     dropout_rate: float = 0.1
     activation: Callable = F.gelu
     norm_first: bool = False
--- model/tiny_transformer.py
+++ model/tiny_transformer.py
@@ -1,9 +1,36 @@
+import torch
 from torch import nn
+
+from model.decoder import DecoderLayer
+
+from .model_config import ModelConfig
+from .encoder import EncoderLayer
 
 
 class TinyTransformer(nn.Module):
-    def __init__(self):
+    def __init__(self, config: ModelConfig):
         super(TinyTransformer, self).__init__()
+        self.config = config
+        self.encoder_layers = nn.ModuleList(
+            [EncoderLayer(config) for _ in range(config.num_layers)]
+        )
+        self.decoder_layers = nn.ModuleList(
+            [DecoderLayer(config) for _ in range(config.num_layers)]
+        )
+        self.encoder_embedding = nn.Embedding(config.num_embeddings, config.num_embeddings)
+        self.decoder_embedding = nn.Embedding(config.num_embeddings, config.num_embeddings)
+        self.fc_out = nn.Linear(config.num_embeddings, config.num_embeddings)
 
-    def forward(self, x):
-        pass
+    def forward(self, x: torch.Tensor, targets: torch.Tensor | None = None):
+        encoder_output = self.encoder_embedding(x)
+        for layer in self.encoder_layers:
+            encoder_output = layer(encoder_output)
+
+        if targets is None:
+            return encoder_output
+
+        decoder_output = self.decoder_embedding(targets)
+        for layer in self.decoder_layers:
+            decoder_output = layer(decoder_output, encoder_output)
+        output = self.fc_out(decoder_output)
+        return output
