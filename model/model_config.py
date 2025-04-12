cc4f871142a35f4a918803589fcb48272e982099
Ivan Chukhran <ivan.chuhran@gmail.com>
2025-04-13 02:40:58 +0300

    update formatting rules, format files

--- model/model_config.py
+++ model/model_config.py
@@ -1,7 +1,6 @@
 from typing import Callable
 
 from pydantic import BaseModel, ConfigDict
-
 from torch.nn import functional as F
 
 
--- model/tiny_transformer.py
+++ model/tiny_transformer.py
@@ -1,12 +1,10 @@
 import torch
-from torch import nn
 import torch.nn.functional as F
-
+from torch import nn
 
 from .decoder import DecoderLayer
-
-from .model_config import ModelConfig
 from .encoder import EncoderLayer
+from .model_config import ModelConfig
 
 
 class TinyTransformer(nn.Module):
@@ -16,33 +14,19 @@ class TinyTransformer(nn.Module):
 
         self.token_embedding = nn.Embedding(config.vocab_size, config.n_embd)
         self.positional_encoding = nn.Embedding(config.block_size, config.n_embd)
-        # self.positional_encoding = PositionalEncoding(config)
 
-        self.encoder_layers = nn.ModuleList(
-            [EncoderLayer(config) for _ in range(config.n_layers)]
-        )
-        self.decoder_layers = nn.ModuleList(
-            [DecoderLayer(config) for _ in range(config.n_layers)]
-        )
+        self.encoder_layers = nn.ModuleList([EncoderLayer(config) for _ in range(config.n_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(config) for _ in range(config.n_layers)])
 
-        # self.encoder_embedding = nn.Embedding(config.n_embd, config.n_embd)
-        # self.decoder_embedding = nn.Embedding(config.n_embd, config.n_embd)
         self.fc_out = nn.Linear(config.n_embd, config.vocab_size)
 
-    def forward(
-        self, x: torch.Tensor, targets: torch.Tensor | None = None
-    ) -> tuple[torch.Tensor, torch.Tensor | None]:
+    def forward(self, x: torch.Tensor, targets: torch.Tensor | None = None) -> tuple[torch.Tensor, torch.Tensor | None]:
         B, T = x.shape
 
-        # print("x.shape", x.shape, "target shape", targets.shape)
-
-        pos_embd = self.positional_encoding(
-            torch.arange(T, device=x.device, dtype=torch.long)
-        )
+        pos_embd = self.positional_encoding(torch.arange(T, device=x.device, dtype=torch.long))
         tok_embd = self.token_embedding(x)
         x = tok_embd + pos_embd
 
-        # encoder_output = self.encoder_embedding(x.long())
         encoder_output = x
         for layer in self.encoder_layers:
             encoder_output = layer(encoder_output)
@@ -52,8 +36,6 @@ class TinyTransformer(nn.Module):
         if targets is None:
             return encoder_output, loss
 
-        # decoder_output = self.positional_encoding(targets)
-        # decoder_output = self.decoder_embedding(decoder_output)
         decoder_output = encoder_output
         for layer in self.decoder_layers:
             decoder_output = layer(decoder_output, encoder_output)
